5f678b5e9d5a299d483db0925c013bd2517a8e70
Kenneth Glover <kenzo2402@hotmail.com>
2026-09-23 14:57:03 +0900

    Update print statement from 'Hello' to 'Goodbye'
    Fixed bloating file problem.  The array string was declared with assign.

--- Console Bulletin 2.cpp
+++ Console Bulletin 2.cpp
@@ -2302,7 +2302,8 @@ int main()
 				outFile55.open(current_filename, wios::out, ios::trunc);
 				for (int x = 0; x < 257; x++)// To get you all the lines.
 				{
-					//tempFileLines2[x].erase(std::remove(tempFileLines2[x].begin(), tempFileLines2[x].end(), '\0'), tempFileLines2[x].end());
+					// Remove all trailing NULL characters
+					tempFileLines2[x].erase(std::remove(tempFileLines2[x].begin(), tempFileLines2[x].end(), '\0'), tempFileLines2[x].end());
 					outFile55 << tempFileLines2[x];
 					//outFile55 << "\r\n";
 				}
@@ -2431,7 +2432,7 @@ int main()
 
 				for (int x = 0; x < 257; x++) {
 					// Remove NULL characters before writing
-					//tempFileLines[x].erase(std::remove(tempFileLines[x].begin(), tempFileLines[x].end(), '\0'), tempFileLines[x].end());
+					tempFileLines[x].erase(std::remove(tempFileLines[x].begin(), tempFileLines[x].end(), '\0'), tempFileLines[x].end());
 					outFile50 << tempFileLines[x];
 					//outFile50 << "\n"; // Add a newline explicitly
 				}
@@ -2839,7 +2840,7 @@ int main()
 					for (int x = 0; x < 257; x++)
 					{
 						// Remove NULL characters before writing
-						//tempFileLines[x].erase(std::remove(tempFileLines[x].begin(), tempFileLines[x].end(), '\0'), tempFileLines[x].end());
+						tempFileLines3[x].erase(std::remove(tempFileLines[x].begin(), tempFileLines[x].end(), '\0'), tempFileLines[x].end());
 						outFile22 << tempFileLines3[x];
 						outFile22 << "\r\n";
 					}
@@ -2946,9 +2947,9 @@ int main()
 						// insert all the posts to the appropriate position
 						for (int x = 0; x < 257; x++) {
 							// Remove NULL characters before writing
-							//tempFileLines[x].erase(std::remove(tempFileLines[x].begin(), tempFileLines[x].end(), '\0'), tempFileLines[x].end());
+							tempFileLines3[x].erase(std::remove(tempFileLines3[x].begin(), tempFileLines3[x].end(), '\0'), tempFileLines3[x].end());
 							outFile51 << tempFileLines3[x];
-							//outFile50 << "\n"; // Add a newline explicitly
+							outFile51 << "\n"; // Add a newline explicitly
 						}
 
 						outFile51.close();
@@ -3023,9 +3024,9 @@ int main()
 						// insert all the posts to the appropriate position
 						for (int x = 0; x < 257; x++)
 						{
-							tempFileLines[x] += (tempFileLines3[x]);
-
-							outFile << tempFileLines[x];
+							//tempFileLines[x] += (tempFileLines3[x]);
+							tempFileLines3[x].erase(std::remove(tempFileLines3[x].begin(), tempFileLines3[x].end(), '\0'), tempFileLines3[x].end());
+							outFile << tempFileLines3[x];
 							outFile << "\r\n";
 						}
 						outFile.close();
